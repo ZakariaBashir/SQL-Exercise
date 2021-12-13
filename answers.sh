12feb9045fe2559bc3e4108a1db34c9952d20a44
zakaria bashir <zakaria.bashir98@gmail.com>
2021-12-13 16:10:33 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,17 @@ SELECT Points FROM students WHERE name="Alex";
 INSERT into students VALUES(7,"Zakaria","23","M",999);
 UPDATE students SET Points=350 WHERE name="Basma";
 UPDATE students SET Points=180 WHERE name="Alex";
+
+CREATE TABLE graduates(
+ID INTEGER PRIMARY KEY NOTNULL AUTOINCREMENT, 
+Name TEXT UNIQUE NOTNULL,
+ Age INTEGER,
+ Gender TEXT,
+ Points INTEGER,
+ Graduation TEXT
+ );
+
+insert into graduates (ID,Name,Age,Gender,Points) select ID, name, Age, Gender, Points  from students where ID=4;
+SELECT * from graduates;
+UPDATE graduates SET Graduation="08/09/2018" WHERE ID=4;
+DELETE from students where ID=4;
